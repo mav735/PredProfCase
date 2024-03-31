19841ae03361ec20705a1a9ae2967a2d379d44fe
Arseny <20096ninja@gmail.com>
2024-03-31 13:07:15 +0300

    app update

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
-from flask import Flask
+import json
+
+from flask import Flask, jsonify
+from flask import Flask, request
 import requests
 
-import request
+import request as req
 from data.data_base_requests import *
 
 app = Flask(__name__)
 
 
 def initialize_db():
-    request = requests.get('https://olimp.miet.ru/ppo_it_final/date', headers={'X-Auth-Token': 'ppo_11_30020'}).json()
-    for i in request['message']:
+    r = requests.get('https://olimp.miet.ru/ppo_it_final/date', headers={'X-Auth-Token': 'ppo_11_30020'}).json()
+    for i in r['message']:
         date = i.split('-')
         date_data = requests.get(f'https://olimp.miet.ru/ppo_it_final?day={date[0]}&month={date[1]}&year={date[2]}',
                                  headers={'X-Auth-Token': 'ppo_11_30020'}).json()
         date_data['message']['date']['data'] = i
+        print(date_data)
         if get_date(i) is None:
-            add(i, str(date_data).replace(' ', ''))
+            add(i, str(date_data).replace(' ', '').replace("True", "1").replace("False", "0").replace("'", '"'))
 
 
 @app.route('/')
 def index():
-    date_data = requests.get("https://olimp.miet.ru/ppo_it_final?day=25&month=01&year=23", headers={'X-Auth-Token': 'ppo_11_30020'}).json()['message']
-    return jsonify(request.json_processing(date_data))
+    date = request.args.get('date', '25-01-23')
+
+    return jsonify(req.json_processing(json.loads(get_date(date).values)))
 
 
 if __name__ == '__main__':
