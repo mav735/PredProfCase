f1a388a91232bfa20bc6a3404358ffc6bbd48911
davydovil <dawydowi@ya.rul>
2024-03-31 11:29:54 +0300

    database models

---
+++ models/date.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+
+from data.db_session import SqlAlchemyBase
+
+
+class Date(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'date'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                               primary_key=True,
+                               autoincrement=True)
+    date = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    count_floors = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    id_floors = sqlalchemy.Column(sqlalchemy.String, nullable=True)
