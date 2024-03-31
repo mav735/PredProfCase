4a5cd887aee4d37366b037312cab333644d3afd2
Fedor Lebed <flebedev777@gmail.com>
2024-03-31 13:31:10 +0300

    added put request

--- data/data_base_requests.py
+++ data/data_base_requests.py
@@ -18,3 +18,10 @@ def add(date, info):
     session.add(date1)
     session.commit()
     session.close()
+
+
+def put(date, info):
+    session = db_session.create_session()
+    date0 = get_date(date)
+    setattr(date0, 'values', info)
+    session.commit()
