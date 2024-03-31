43aa89bd43b69a402a831aa2d85a8f84ce1afaa4
Arseny <20096ninja@gmail.com>
2024-03-31 13:08:42 +0300

    gitignore update

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ def initialize_db():
         date_data = requests.get(f'https://olimp.miet.ru/ppo_it_final?day={date[0]}&month={date[1]}&year={date[2]}',
                                  headers={'X-Auth-Token': 'ppo_11_30020'}).json()
         date_data['message']['date']['data'] = i
-        print(date_data)
         if get_date(i) is None:
-            add(i, str(date_data).replace(' ', '').replace("True", "1").replace("False", "0").replace("'", '"'))
+            add(i, str(date_data).replace(' ', '').replace("True", "1").replace(
+                "False", "0").replace("'", '"'))
 
 
 @app.route('/')
